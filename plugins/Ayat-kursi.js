120cd4b80754ea0a1446954bd3c2adfbb3e05fa3
Devil-08 <128286120+Devil-08@users.noreply.github.com>
2023-05-28 04:47:09 +0530

    Update Ayat-kursi.js

--- plugins/Ayat-kursi.js
+++ plugins/Ayat-kursi.js
@@ -3,20 +3,18 @@ let handler = async (m, { conn }) => {
   let caption = `
 *「 Ayat Kursi 」*
 
-اللَّهُ لَا إِلَهَ إِلَّا هُوَ الْحَيُّ الْقَيُّومُ لَا تَأْخُذُهُ سِنَةٌ وَلَا نَوْمٌ لَهُ مَا فِي السَّمَاوَاتِ وَمَا فِي الْأَرْضِ مَنْ ذَا الَّذِي يَشْفَعُ عِنْدَهُ إِلَّا بِإِذْنِهِ يَعْلَمُ مَا بَيْنَ أَيْدِيهِمْ وَمَا خَلْفَهُمْ وَلَا يُحِيطُونَ بِشَيْءٍ مِنْ عِلْمِهِ إِلَّا بِمَا شَاءَ وَسِعَ كُرْسِيُّهُ السَّمَاوَاتِ وَالْأَرْضَ وَلَا يَئُودُهُ حِفْظُهُمَا وَهُوَ الْعَلِيُّ الْعَظِيمُ
+"जय श्रीं राम 🚩, राम ही सर्वस्व हैं 😌"
 
-“Alloohu laa ilaaha illaa huwal hayyul qoyyuum, laa ta’khudzuhuu sinatuw walaa naum. Lahuu maa fissamaawaati wa maa fil ardli man dzal ladzii yasyfa’u ‘indahuu illaa biidznih, ya’lamu maa baina aidiihim wamaa kholfahum wa laa yuhiithuuna bisyai’im min ‘ilmihii illaa bimaa syaa’ wasi’a kursiyyuhus samaawaati wal ardlo walaa ya’uuduhuu hifdhuhumaa wahuwal ‘aliyyul ‘adhiim.”
-
+“JAI SHREE RAM 🚩,RAM IS EVERTHING 😌"
 
 Artinya:
-Allah, tidak ada Tuhan (yang berhak disembah) melainkan Dia Yang Hidup kekal lagi terus menerus mengurus (makhluk-Nya); tidak mengantuk dan tidak tidur. Kepunyaan-Nya apa yang di langit dan di bumi. Tiada yang dapat memberi syafa'at di sisi Allah tanpa izin-Nya.
-Allah mengetahui apa-apa yang di hadapan mereka dan di belakang mereka, dan mereka tidak mengetahui apa-apa dari ilmu Allah melainkan apa yang dikehendaki-Nya. Kursi Allah meliputi langit dan bumi. Dan Allah tidak merasa berat memelihara keduanya, dan Allah Maha Tinggi lagi Maha Besar." 
-(QS. Al Baqarah: 255)
+RAM RAM RAM RAM 😌
+(QS. Al siya ram : 255)
 `.trim()
   m.reply(caption)
 }
-handler.help = ['ayatkursi']
-handler.tags = ['quran', 'limitmenu']
+handler.help = ['ram']
+handler.tags = ['jai', 'limitmenu']
 handler.command = /^(ayatkursi)$/i
 handler.limit = true
 
